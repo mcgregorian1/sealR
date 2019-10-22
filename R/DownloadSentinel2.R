0d417ff19f8ab2740f64d632f72f044b99bd59a6
Xiaojie Gao <xgao26@wolftech.ad.ncsu.edu>
2019-10-22 11:20:57 -0400

    fix download sentinel 2

--- R/DownloadSentinel2.R
+++ R/DownloadSentinel2.R
@@ -1,23 +1,24 @@
 #' Download Sentinel 2 L1C dataset from google cloud.
-#' Author: Jacory
-#' 
+#'
 #' This function requires Google Cloud Storage SDK, you can download and install this SDK
 #' from google cloud website.
-#' 
+#'
 #' Once it started, press ctrl+c in the command line can stop it.
-#' 
+#'
 #' If there's any bug, please let me know.
+#'
+#' Author: Jacory
 #'
 #' @param tilename what tile do you want to download
 #' @param folder local folder used to store downloaded files
 #' @param L2Flag indicate to download L2A product or L1C product (default)
-#' @param regExp regular expression, with which you can speicify which files you want. For example, 
+#' @param regExp regular expression, with which you can speicify which files you want. For example,
 #' regExp <- "S2\\D_MSIL\\d\\D_201([6]|[8][0][0-9]).*.SAFE/$" means you want data from 2016 and before 2018 before October.
 #' @keywords sentinel
 #' @export
 #' @examples
-#' DownloadSentinel2L1C("17SNA")
-DownloadSentinel2L1C <- function(tilename, folder, L2Flag = FALSE, regExp = NULL) {
+#' DownloadSentinel2("17SNA", "D:/", L2Flag = TRUE, rexExp = "S2\\D_MSIL\\d\\D_201([6]|[8][0][0-9]).*.SAFE/$")
+DownloadSentinel2 <- function(tilename, folder, L2Flag = FALSE, regExp = NULL) {
     gcUrl <- ifelse(L2Flag == TRUE, file.path("gs://gcp-public-data-sentinel-2/L2/tiles", substr(tilename, 1, 2),
                                         substr(tilename, 3, 3), substr(tilename, 4, 5)),
                                     file.path("gs://gcp-public-data-sentinel-2/tiles", substr(tilename, 1, 2),
